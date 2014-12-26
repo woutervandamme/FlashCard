5863d0257934e210ffe72a878db92d2f065a8d19
robbe.roels@gmail.com <J@j0eCS252365_>
2014-12-27 00:36:02 +0100

    All database gets done
    Still need to test them.

--- app/src/main/java/wouter/vandamme/robbe/roels/flashcard/domain/JSONWebDB.java
+++ app/src/main/java/wouter/vandamme/robbe/roels/flashcard/domain/JSONWebDB.java
@@ -16,6 +16,7 @@ import domain.Group;
 import domain.Message;
 import domain.MessageType;
 import domain.Question;
+import domain.QuestionFactory;
 import domain.QuestionImage;
 import domain.QuestionText;
 import domain.User;
@@ -59,7 +60,7 @@ public class JSONWebDB implements Database {
 
     @Override
     public Question getRandomQuestion(int id) throws DBException {
-        String sql = "select * from QuestionsInGroup where gropuID = ?";
+        String sql = "select * from QuestionsInGroup where groupID = ?";
         String params = "line="+sql+"&vals="+id+"&types="+"i";
         DatabaseTask dbTask = new DatabaseTask();
         dbTask.execute(url,params);
@@ -181,23 +182,120 @@ public class JSONWebDB implements Database {
     }
 
     @Override
-    public ArrayList<Group> getGroupsForUser(String s) throws DBException {
-        return null;
+    public ArrayList<Group> getGroupsForUser(String email) throws DBException {
+        String sql = "select * from UsersInGroup where userID = ?";
+        String params = "line="+sql+"&vals="+email+"&types="+"s";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        ArrayList<Group> groups = new ArrayList<Group>();
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    Group g = getGroup(row.getInt("GroupID"));
+                    groups.add(g);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return groups;
     }
 
     @Override
-    public User getGroupAdmin(int i) throws DBException {
-        return null;
+    public User getGroupAdmin(int id) throws DBException {
+        String sql = "select admin from Group where ID = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        User admin = null;
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    admin = getUser(row.getString("UserID"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return admin;
     }
 
     @Override
-    public Group getGroup(int i) throws DBException {
-        return null;
+    public Group getGroup(int id) throws DBException {
+        String sql = "select * from group where id = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        Group group = null;
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    User u = getUser(row.getString("admin"));
+                    group = new Group(u,row.getString("name"),row.getBoolean("userCanInvite"),row.getBoolean("userCanAddQuestions"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return group;
+
+
     }
 
     @Override
-    public Question getQuestion(int i) throws DBException {
-        return null;
+    public Question getQuestion(int id) throws DBException {
+        String sql = "select * from question where id = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        Question question = null;
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    question = QuestionFactory.getQuestion(row.getString("answer"),row.getString("question"),row.getString("type"));
+                    question.setExtraInfo(row.getString("extrainfo"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (DomainException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return question;
     }
 
     @Override
