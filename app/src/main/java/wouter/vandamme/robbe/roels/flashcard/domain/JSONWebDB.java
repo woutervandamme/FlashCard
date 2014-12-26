00c28f0bfae2f0d82670bfbd980c4207e6fae92e
robbe.roels@gmail.com <J@j0eCS252365_>
2014-12-27 00:08:40 +0100

    Partly implemented JSONWebDB
    Work in progress.

--- app/src/main/java/wouter/vandamme/robbe/roels/flashcard/domain/JSONWebDB.java
+++ app/src/main/java/wouter/vandamme/robbe/roels/flashcard/domain/JSONWebDB.java
@@ -7,12 +7,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import db.DBException;
 import db.Database;
+import domain.DomainException;
 import domain.Group;
 import domain.Message;
+import domain.MessageType;
 import domain.Question;
+import domain.QuestionImage;
+import domain.QuestionText;
 import domain.User;
 
 /**
@@ -53,23 +58,126 @@ public class JSONWebDB implements Database {
     }
 
     @Override
-    public Question getRandomQuestion(int i) throws DBException {
-        return null;
+    public Question getRandomQuestion(int id) throws DBException {
+        String sql = "select * from QuestionsInGroup where gropuID = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        Question q = null;
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                Random r = new Random();
+                int rowNumber = (int) (json.length() * r.nextFloat());
+
+                JSONObject row = json.getJSONObject(rowNumber);
+                int questionID = row.getInt("QuestionID");
+                q = this.getQuestion(questionID);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return q;
     }
 
     @Override
-    public Message getMessage(int i) throws DBException {
-        return null;
+    public Message getMessage(int id) throws DBException {
+        String sql = "select * from Message where ID = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        Message m = null;
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    User u = getUser(row.getString("UserID"));
+                    m = new Message(id,row.getString("title"),row.getString("body"), MessageType.toMessageType(row.getString("type")),u);
+                 }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (DomainException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return m;
+
     }
 
     @Override
-    public ArrayList<User> getUsersFromGroup(int i) throws DBException {
-        return null;
+    public ArrayList<User> getUsersFromGroup(int id) throws DBException {
+        String sql = "select * from UsersInGroup where groupID = ?";
+        String params = "line="+sql+"&vals="+id+"&types="+"i";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        ArrayList<User> users = new ArrayList<User>();
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    User u = getUser(row.getString("UserID"));
+                    users.add(u);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return users;
     }
 
     @Override
-    public ArrayList<Message> getMessages(String s) throws DBException {
-        return null;
+    public ArrayList<Message> getMessages(String email) throws DBException {
+        String sql = "select * from Message where UserID = ? ";
+        String params = "line="+sql+"&vals="+email+"&types="+"s";
+        DatabaseTask dbTask = new DatabaseTask();
+        dbTask.execute(url,params);
+        ArrayList<Message> messages = new ArrayList<Message>();
+        while (!dbTask.done()) {
+        }
+        if (dbTask.fetched()) {
+            JSONArray json;
+            Log.v("DATA", "String is: '" + dbTask.getJsonString() + "'");
+            try {
+                json = new JSONArray(dbTask.getJsonString());
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject row = json.getJSONObject(i);
+                    User u = getUser(row.getString("UserID"));
+                    Message m = new Message(row.getInt("ID"),row.getString("title"),row.getString("body"), MessageType.toMessageType(row.getString("type")),u);
+                    messages.add(m);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (DomainException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Log.v("DATA", "NOT FETCHED" );
+        }
+        return messages;
     }
 
     @Override
