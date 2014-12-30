c879ad34f0c84286ade0b982301755b0830be695
Wouter Van Damme <wouterr.van.damme@gmail.com>
2014-12-30 20:04:25 +0100

    added error  handling to EditText

--- app/src/main/java/wouter/vandamme/robbe/roels/flashcard/views/CustomActivity.java
+++ app/src/main/java/wouter/vandamme/robbe/roels/flashcard/views/CustomActivity.java
@@ -2,7 +2,10 @@ package wouter.vandamme.robbe.roels.flashcard.views;
 
 import android.app.Activity;
 import android.content.Context;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import wouter.vandamme.robbe.roels.flashcard.R;
 
 /**
  * Created by wouter on 29/12/14.
@@ -15,6 +18,13 @@ public class CustomActivity extends Activity {
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context,message, duration);
         toast.show();
+    }
+
+
+    public void checkInputVerification(EditText text){
+        if(text.getText().toString() == null || text.getText().toString().isEmpty()){
+            text.setError(getResources().getString(R.string.errorEmptyField));
+        }
     }
 
 }
